5771f9d8b2f75be794497189514973cc3e95a088
Jérôme Benoit <jerome.benoit@sap.com>
2025-11-27 22:12:00 +0100

    fix: ssh config for macOS
    Signed-off-by: Jérôme Benoit <jerome.benoit@sap.com>

--- home-manager/modules/ssh.nix
+++ home-manager/modules/ssh.nix
@@ -1,17 +1,31 @@
-{ pkgs, ... }:
+{ pkgs, lib, ... }:
 let
   systemSsh = pkgs.runCommand "ssh-system" { } "mkdir -p $out";
 in
 {
-  programs.ssh = {
-    enable = true;
-    package = systemSsh;
-    matchBlocks = {
-      "*" = {
-        addKeysToAgent = "yes";
-        forwardAgent = true;
-        forwardX11 = true;
+  programs.ssh = lib.mkMerge [
+    {
+      enable = true;
+      package = systemSsh;
+      matchBlocks = {
+        "*" = {
+          addKeysToAgent = "yes";
+          forwardAgent = true;
+          forwardX11 = true;
+        };
       };
-    };
-  };
+    }
+    (lib.mkIf pkgs.stdenv.isDarwin {
+      matchBlocks = {
+        "*" = {
+          extraOptions = {
+            UseKeychain = "yes";
+          };
+        };
+        "*.local" = {
+          user = "fraggle";
+        };
+      };
+    })
+  ];
 }
