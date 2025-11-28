c3f9526b46f48022958ee573fe40a3b3b8369554
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-11-28 02:10:53 +0100

    refactor: handle .signature file
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- home-manager/home.nix
+++ home-manager/home.nix
@@ -39,33 +39,57 @@
 
   specialisation = {
     work = {
-      configuration = {
-        programs.git.settings.user = {
-          email = lib.mkForce "jerome.benoit@sap.com";
-          signingKey = lib.mkForce "27B535D3";
-        };
+      configuration =
+        let
+          gpgKeyId = "27B535D3";
+          gpgFingerprint = "B799 BBF6 8EC8 911B B8D7 CDBC C3B1 92C6 27B5 35D3";
+        in
+        {
+          home.file.".signature".text = lib.mkForce ''
+            Jérôme Benoit - R&D Software Engineer
+            SAP Labs France
+            OpenPGP Key ID : ${gpgKeyId}
+            Key fingerprint : ${gpgFingerprint}
+          '';
 
-        programs.zsh.shellAliases = {
-          hm = lib.mkForce "nh home switch --specialisation work";
-          hmw = "nh home switch --specialisation work";
-          hmp = "nh home switch --specialisation personal";
+          programs.git.settings.user = {
+            email = lib.mkForce "jerome.benoit@sap.com";
+            signingKey = lib.mkForce gpgKeyId;
+          };
+
+          programs.zsh.shellAliases = {
+            hm = lib.mkForce "nh home switch --specialisation work";
+            hmw = "nh home switch --specialisation work";
+            hmp = "nh home switch --specialisation personal";
+          };
         };
-      };
     };
 
     personal = {
-      configuration = {
-        programs.git.settings.user = {
-          email = lib.mkForce "jerome.benoit@piment-noir.org";
-          signingKey = lib.mkForce "27B535D3";
-        };
+      configuration =
+        let
+          gpgKeyId = "27B535D3";
+          gpgFingerprint = "B799 BBF6 8EC8 911B B8D7 CDBC C3B1 92C6 27B5 35D3";
+        in
+        {
+          home.file.".signature".text = lib.mkForce ''
+            Jérôme Benoit aka fraggle
+            Piment Noir - https://piment-noir.org
+            OpenPGP Key ID : ${gpgKeyId}
+            Key fingerprint : ${gpgFingerprint}
+          '';
 
-        programs.zsh.shellAliases = {
-          hm = lib.mkForce "nh home switch --specialisation personal";
-          hmw = "nh home switch --specialisation work";
-          hmp = "nh home switch --specialisation personal";
+          programs.git.settings.user = {
+            email = lib.mkForce "jerome.benoit@piment-noir.org";
+            signingKey = lib.mkForce gpgKeyId;
+          };
+
+          programs.zsh.shellAliases = {
+            hm = lib.mkForce "nh home switch --specialisation personal";
+            hmw = "nh home switch --specialisation work";
+            hmp = "nh home switch --specialisation personal";
+          };
         };
-      };
     };
   };
 }
