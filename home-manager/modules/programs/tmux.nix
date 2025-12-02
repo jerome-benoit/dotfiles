e3524da837f70c9b7eb4858a4061eae4807840b2
Jérôme Benoit <jerome.benoit@piment-noir.org>
2025-12-03 00:36:03 +0100

    fix(tmux): terminal overrides
    Signed-off-by: Jérôme Benoit <jerome.benoit@piment-noir.org>

--- home-manager/modules/programs/tmux.nix
+++ home-manager/modules/programs/tmux.nix
@@ -45,7 +45,7 @@ in
 
         # Terminal Overrides
         set -as terminal-overrides ',*:Smulx=\E[4::%p1%dm'
-        set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
+        set -as terminal-overrides ',*:Setulc=\E[58::2::%p1%{65536}%/%d::%p1%{256}%/%{255}%&%d::%p1%{255}%&%d%;m'
 
         # ============================================================================
         # KEY BINDINGS
