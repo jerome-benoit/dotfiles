adecbbf269c623b12dd126ff00f5b99174f1eaa9
Jérôme Benoit <jerome.benoit@sap.com>
2026-03-29 13:44:32 +0200

    fix: remove accidental result symlink, add to gitignore

--- home-manager/modules/development/openspec.nix
+++ home-manager/modules/development/openspec.nix
@@ -8,20 +8,6 @@
 let
   cfg = config.modules.development.openspec;
   system = pkgs.stdenv.hostPlatform.system;
-
-  nodejs_20 = pkgs.nodejs_20.override {
-    nodejs-slim = pkgs.nodejs-slim_20.overrideAttrs { doCheck = false; };
-  };
-  openspecBase = inputs.openspec.packages.${system}.default or null;
-  openspec =
-    if openspecBase != null then
-      openspecBase.overrideAttrs (old: {
-        nativeBuildInputs = map (drv: if (drv.pname or "") == "nodejs" then nodejs_20 else drv) (
-          old.nativeBuildInputs or [ ]
-        );
-      })
-    else
-      null;
 in
 {
   options.modules.development.openspec = {
@@ -29,7 +15,7 @@ in
 
     openspecPackage = lib.mkOption {
       type = lib.types.nullOr lib.types.package;
-      default = openspec;
+      default = inputs.openspec.packages.${system}.default or null;
       defaultText = lib.literalExpression "inputs.openspec.packages.\${system}.default";
       description = "OpenSpec CLI package";
       example = lib.literalExpression "inputs.openspec.packages.\${system}.default";
