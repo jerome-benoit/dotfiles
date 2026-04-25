1c76495a66352e656a02cffe4ed42878e67daca0
Jérôme Benoit <jerome.benoit@sap.com>
2026-04-25 21:55:54 +0200

    fix(darwin): skip direnv checkPhase to dodge nix codesigning bug
    direnv tests invoke fish which gets SIGKILLed by the kernel due to
    Nix RewritingSink corrupting Mach-O page hashes on sibling outputs.
    
    Drop once NixOS/nix#15638 lands.
    
    Ref: https://github.com/NixOS/nixpkgs/issues/511265

--- home-manager/modules/shell/direnv.nix
+++ home-manager/modules/shell/direnv.nix
@@ -1,12 +1,14 @@
 {
   config,
   lib,
+  pkgs,
   ...
 }:
 
 let
   cfg = config.modules.shell.direnv;
   mkPlatformPackage = config.modules.core.lib.mkPlatformPackage;
+  isDarwin = pkgs.stdenv.hostPlatform.isDarwin;
 in
 {
   options.modules.shell.direnv = {
@@ -16,7 +18,10 @@ in
   config = lib.mkIf cfg.enable {
     programs.direnv = {
       enable = true;
-      package = mkPlatformPackage "direnv" { };
+      # https://github.com/NixOS/nix/pull/15638
+      package = (mkPlatformPackage "direnv" { }).overrideAttrs (old: {
+        doCheck = (old.doCheck or true) && !isDarwin;
+      });
       nix-direnv.enable = true;
       enableZshIntegration = false;
     };
