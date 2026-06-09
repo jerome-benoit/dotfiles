2f7b341d6df52b8dc528dd262d3d9a75fc8cbbee
Jérôme Benoit <jerome.benoit@piment-noir.org>
2026-06-09 15:11:01 +0200

    fix: update agent-deck vendor hash

--- home-manager/modules/development/agent-deck.nix
+++ home-manager/modules/development/agent-deck.nix
@@ -129,7 +129,7 @@ let
     version = config.modules.core.lib.mkUnstableVersion inputs.agent-deck;
     src = inputs.agent-deck;
 
-    vendorHash = "sha256-ltU0qyZEUjzN+E5FOBnfnc4W3CchPJ0+0GFCtA9C8Zo="; # @ci:vendor-hash
+    vendorHash = "sha256-GyG71/iR2R4mq1vOYcL4rGXh0RQIMNeWj+WtjF75KCg="; # @ci:vendor-hash
     subPackages = [ "cmd/agent-deck" ];
 
     ldflags = [
